5565984a321051a46507bdfde720d0050de8be71
Phil-1041 <philzheng92@gmail.com>
2019-11-18 04:48:57 -0800

    before react

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Pizza"));
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User')
+const bodyParser = require('body-parser')
+const passport = require('passport');
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+  enxtended: false
+}));
+
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
+// app.get("/", (req, res) => {
+//   const user = new User({
+//     handle: 'phil',
+//     email: 'phil@email.com',
+//     password: 'philisgreat123'
+//   })
+//   user.save()
+//   res.send("Hello World")
+// });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
